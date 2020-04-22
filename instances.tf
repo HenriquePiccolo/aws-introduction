2404ec87e8f3f240dfd9bcfbd2c125e1444d4515
henriquepiccolo <henriquepiccolo@hotmail.com.br>
2020-04-22 13:37:16 -0300

    Aula 2

--- instances.tf
+++ instances.tf
@@ -8,7 +8,9 @@ resource "aws_instance" "instances" {
 
   key_name = aws_key_pair.key_pair.key_name
 
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id, aws_security_group.database.id]
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id,aws_security_group.database.id,aws_security_group.allow_outbound.id, aws_security_group.load_balancer_tg.id]
+
+  user_data = templatefile("${path.module}/user_data/install_app.tmpl", {database_endpoint = module.rds.this_db_instance_endpoint})
 
   tags = {
       Name = "zup_instances"
---
+++ lambda.tf
@@ -0,0 +1,4 @@
+data "aws_s3_bucket" "bucket_slack_lambda"{
+    bucket = "zup-s3-01"
+    
+}
